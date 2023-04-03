45ca38728e14c83f93ce6324216db658b1987d26
Awaab Abdelbagi <92119191+2waab@users.noreply.github.com>
2023-04-03 14:50:10 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,32 @@
-let menuIcon = document.querySelector(".menuIcon");
+let menuIconAside = document.querySelector(".menuIcon");
+let menuIconHead = document.querySelector(".menuIcon-haed");
 let asideSection = document.querySelector("aside");
 let header = document.querySelector(".header");
 let headerFirst = document.querySelector(".first");
 let userInfo = document.querySelector(".content-of-page");
+let links = document.querySelectorAll(".links ul li");
 
-menuIcon.addEventListener("click", () => {
-    menuIcon.classList.toggle("open");
-    asideSection.classList.toggle("open");
-    header.classList.toggle("open");
-    headerFirst.classList.toggle("open");
-    userInfo.classList.toggle("open");
+menuIconAside.addEventListener("click", () => {
+    menuIconAside.classList.add("close");
+    asideSection.classList.add("close");
+    header.classList.add("close");
+    headerFirst.classList.add("close");
+    userInfo.classList.add("close");
+});
+
+menuIconHead.addEventListener("click", () => {
+    menuIconAside.classList.remove("close");
+    asideSection.classList.remove("close");
+    header.classList.remove("close");
+    headerFirst.classList.remove("close");
+    userInfo.classList.remove("close");
+});
+
+links.forEach((li) => {
+    li.addEventListener("click", () => {
+        links.forEach((link) => {
+            link.classList.remove("active");
+            li.classList.add("active");
+        });
+    });
 });
